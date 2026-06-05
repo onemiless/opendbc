eb74d9fe5aba465957598cb4ec389e25a3ae218d
onemiles <690299233@qq.com>
2026-06-05 20:56:12 +0800

    fix: clean carstate_ext — only 4-finger toggle, remove broken auto-stock

--- opendbc/sunnypilot/car/tesla/carstate_ext.py
+++ opendbc/sunnypilot/car/tesla/carstate_ext.py
@@ -23,25 +23,8 @@ class CarStateExt:
     self.active_touch_points = 0
     self.tesla_stock_longitudinal_active = False
     self.prev_touch_points_for_long = 0
-    self._dyn_frame = 0
-    self._dyn_high = 80
-    self._dyn_low = 70
 
   def update(self, ret: structs.CarState, ret_sp: structs.CarStateSP, can_parsers: dict[StrEnum, CANParser]) -> None:
-    self._dyn_frame += 1
-    if self._dyn_frame % 300 == 0:
-      try:
-        from openpilot.common.params import Params
-        p = Params()
-        if p.get_bool("DynamicAutoStock"):
-          self._dyn_high = p.get_int("DynamicAutoStockSpeedKph", default=80)
-          self._dyn_low = p.get_int("DynamicAutoStockSpeedLowKph", default=70)
-        else:
-          self._dyn_high = 999
-          self._dyn_low = -1
-      except Exception:
-        pass
-
     if self.CP_SP.flags & TeslaFlagsSP.HAS_VEHICLE_BUS:
       cp_adas = can_parsers[Bus.adas]
 
@@ -63,13 +46,6 @@ class CarStateExt:
       self.prev_touch_points_for_long = self.active_touch_points
       if prev_touch_long != 4 and self.active_touch_points == 4:
         self.tesla_stock_longitudinal_active = not self.tesla_stock_longitudinal_active
-
-    # Dynamic auto-stock based on configurable speed thresholds
-    speed_kph = ret.vEgo * CV.MS_TO_KPH
-    if speed_kph > self._dyn_high:
-      self.tesla_stock_longitudinal_active = True
-    elif speed_kph < self._dyn_low:
-      self.tesla_stock_longitudinal_active = False
 
     if self.tesla_stock_longitudinal_active:
       ret_sp.flags |= TeslaFlagsSP.STOCK_LONGITUDINAL_ACTIVE.value
